26420c78d7659c827f1f918505abce4fb062eb3f
naveenkarmegam <naveenkkn03@gmail.com>
2023-11-28 17:57:44 +0530

    first commit

---
+++ src/productLsit.js
@@ -0,0 +1,370 @@
+
+const product_list =  [
+    {
+        id: 1,
+        title: "iPhone 9",
+        description: "An apple mobile which is nothing like apple",
+        price: 549,
+        quantity:1,
+        discountPercentage: 12.96,
+        rating: 4.69,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/1/1.jpg",
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 2,
+        title: "iPhone X",
+        description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+        price: 899,
+        quantity:1,
+        discountPercentage: 17.94,
+        rating: 4.44,
+        stock: 34,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/2/1.jpg",
+            "https://i.dummyjson.com/data/products/2/2.jpg",
+            "https://i.dummyjson.com/data/products/2/3.jpg",
+            "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 3,
+        title: "Samsung Universe 9",
+        description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+        price: 1249,
+        quantity:1,
+        discountPercentage: 15.46,
+        rating: 4.09,
+        stock: 36,
+        brand: "Samsung",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/3/1.jpg"
+        ]
+    },
+    {
+        id: 4,
+        title: "OPPOF19",
+        description: "OPPO F19 is officially announced on April 2021.",
+        price: 280,
+        quantity:1,
+        discountPercentage: 17.91,
+        rating: 3,
+        stock: 123,
+        brand: "OPPO",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/4/1.jpg",
+            "https://i.dummyjson.com/data/products/4/2.jpg",
+            "https://i.dummyjson.com/data/products/4/3.jpg",
+            "https://i.dummyjson.com/data/products/4/4.jpg",
+            "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 5,
+        title: "Huawei P30",
+        description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+        price: 499,
+        quantity:1,
+        discountPercentage: 10.58,
+        rating: 3,
+        stock: 32,
+        brand: "Huawei",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/5/1.jpg",
+            "https://i.dummyjson.com/data/products/5/2.jpg",
+            "https://i.dummyjson.com/data/products/5/3.jpg"
+        ]
+    },
+    {
+        id: 6,
+        title: "iPhone 8",
+        description: "An apple mobile which is nothing like apple",
+        price: 450,
+        quantity:1,
+        discountPercentage: 12.96,
+        rating: 4.6,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+            
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 7,
+        title: "iPhone X Pro",
+        description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+        price: 1099,
+        quantity:1,
+        discountPercentage: 17.94,
+        rating: 4.44,
+        stock: 35,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+        images: [
+            
+            "https://i.dummyjson.com/data/products/2/2.jpg",
+            "https://i.dummyjson.com/data/products/2/3.jpg",
+            "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 8,
+        title: "Samsung 9",
+        description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+        price: 849,
+        quantity:1,
+        discountPercentage: 15.46,
+        rating: 4.09,
+        stock: 36,
+        brand: "Samsung",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/3/1.jpg"
+        ]
+    },
+    {
+        id: 9,
+        title: "OPPOF19 Pro",
+        description: "OPPO F19 is officially announced on April 2021.",
+        price: 280,
+        quantity:1,
+        discountPercentage: 17.91,
+        rating: 3,
+        stock: 123,
+        brand: "OPPO",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+        images: [
+            
+            "https://i.dummyjson.com/data/products/4/2.jpg",
+            "https://i.dummyjson.com/data/products/4/3.jpg",
+            "https://i.dummyjson.com/data/products/4/4.jpg",
+            "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 10,
+        title: "Huawei S6",
+        description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+        price: 399,
+        quantity:1,
+        discountPercentage: 10.58,
+        rating: 3,
+        stock: 32,
+        brand: "Huawei",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+        images: [
+            
+            "https://i.dummyjson.com/data/products/5/2.jpg",
+            "https://i.dummyjson.com/data/products/5/3.jpg"
+        ]
+    },
+    {
+        id: 11,
+        title: "iPhone 9 Pro",
+        description: "An apple mobile which is nothing like apple",
+        price: 549,
+        quantity:1,
+        discountPercentage: 12.96,
+        rating: 4.69,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+         
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+       
+        ]
+    },
+    {
+        id: 12,
+        title: "iPhone 7",
+        description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+        price: 899,
+        quantity:1,
+        discountPercentage: 17.94,
+        rating: 4.44,
+        stock: 34,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+        images: [
+           
+            "https://i.dummyjson.com/data/products/2/3.jpg",
+            "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 13,
+        title: "Samsung Universe 9",
+        description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+        price: 1249,
+        quantity:1,
+        discountPercentage: 15.46,
+        rating: 3,
+        stock: 36,
+        brand: "Samsung",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/3/1.jpg"
+        ]
+    },
+    {
+        id: 14,
+        title: "OPPO 8",
+        description: "OPPO F19 is officially announced on April 2021.",
+        price: 280,
+        quantity:1,
+        discountPercentage: 17.91,
+        rating: 3,
+        stock: 123,
+        brand: "OPPO",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+        images: [
+           
+           
+            "https://i.dummyjson.com/data/products/4/4.jpg",
+            "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 15,
+        title: "Huawei I20",
+        description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+        price: 499,
+        quantity:1,
+        discountPercentage: 10.58,
+        rating: 3,
+        stock: 32,
+        brand: "Huawei",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+        images: [
+            
+            "https://i.dummyjson.com/data/products/5/2.jpg",
+            "https://i.dummyjson.com/data/products/5/3.jpg"
+        ]
+    },
+    {
+        id: 16,
+        title: "iPhone 9",
+        description: "An apple mobile which is nothing like apple",
+        price: 549,
+        quantity:1,
+        discountPercentage: 12.96,
+        rating: 3,
+        stock: 94,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        images: [
+           
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 17,
+        title: "iPhone XR",
+        description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+        price: 899,
+        quantity:1,
+        discountPercentage: 17.94,
+        rating: 3,
+        stock: 34,
+        brand: "Apple",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+        images: [
+           
+            "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 18,
+        title: "Samsung Universe 9",
+        description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+        price: 1249,
+        quantity:1,
+        discountPercentage: 15.46,
+        rating: 3,
+        stock: 36,
+        brand: "Samsung",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+        images: [
+            "https://i.dummyjson.com/data/products/3/1.jpg"
+        ]
+    },
+    {
+        id: 19,
+        title: "OPPOF20",
+        description: "OPPO F19 is officially announced on April 2021.",
+        price: 280,
+        quantity:1,
+        discountPercentage: 17.91,
+        rating: 3,
+        stock: 123,
+        brand: "OPPO",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+        images: [
+           
+            "https://i.dummyjson.com/data/products/4/4.jpg",
+            "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+        ]
+    },
+    {
+        id: 20,
+        title: "Huawei 200",
+        description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+        price: 499,
+        quantity:1,
+        discountPercentage: 10.58,
+        rating: 3,
+        stock: 32,
+        brand: "Huawei",
+        category: "smartphones",
+        thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+        images: [
+            
+            "https://i.dummyjson.com/data/products/5/3.jpg"
+        ]
+    },
+];
+
+
+
+export { product_list};
